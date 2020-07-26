f5a20ae8955258db6873fa9e67e8a7a6d21e6c4c
Jhon Parra <jhonparra939@gmail.com>
2020-07-26 08:37:52 -0500

    label stats

---
+++ label_stats.py
@@ -0,0 +1,34 @@
+##labels stats
+
+import os
+import numpy
+import glob
+import io
+import collections
+DEST_PATH="D:/Jhon Lopez/Tesis Icesi/img/"
+#DEST_PATH="E:/Gdrive/img/"
+
+
+
+files = glob.glob(os.path.join(DEST_PATH, '*.txt'))
+class_aux=open(DEST_PATH+"classes.txt","r")
+classes=[line.strip() for line in class_aux]
+class_aux.close()
+
+marcas=[]
+
+for file in files:
+    with io.open(file, mode="r", encoding="utf-8") as f:
+        try:
+            for line in f:
+                clase,ymin,ymax,xmin,xmax=line.split()
+                marcas.append(classes[int(clase)])
+        except:
+            print(f"Warning: {file} does not contain any valid data")
+      
+##show counts                
+
+print(collections.Counter(marcas))
+
+
+
